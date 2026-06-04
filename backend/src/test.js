f1b7aa2477c2e13bdcc4c5e625c2ffbb954d5599
Wayne <wynnee0110@gmail.com>
2026-06-04 22:12:38 +0800

    upd

---
+++ backend/src/test.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
+
+async function testTMDB() {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/popular",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+}
+
+testTMDB();
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home(){
+    return (
+        <div>
+            <h1>Movies</h1>
+            <iframe 
+                src="https://www.vidking.net/embed/movie/1078605?color=e50914" 
+                width="640" 
+                height="360" 
+                frameBorder="0" 
+                allowFullScreen>
+            </iframe>
+        </div>
+    )
+}
