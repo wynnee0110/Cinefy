eb4fb132a0a9f5a7fadd8f4bcdfc1ead2f10c66a
Wayne <wynnee0110@gmail.com>
2026-06-06 14:59:58 +0800

    upd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Cinefy",
+        short_name: "Cinefy",
+        description: "Movie streaming platform",
+        theme_color: "#000000",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
