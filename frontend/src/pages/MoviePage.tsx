df45ce3dbfacf87071884813aaaa5f2c6ddeb9ae
Wayne <wynnee0110@gmail.com>
2026-06-08 23:29:41 +0800

    upd

--- frontend/src/pages/MoviePage.tsx
+++ frontend/src/pages/MoviePage.tsx
@@ -329,7 +329,7 @@ export default function MoviePage() {
 
     {/* VidKing FULL VIDEO */}
     <iframe
-      src={`https://vidsync.xyz/embed/movie/${movie.id}?color=e50914`}
+      src={`https://vidking.net/embed/movie/${movie.id}`}
       className="w-full h-full"
       allow="autoplay; fullscreen"
       allowFullScreen
