975dc9c6347ea9e91f08bfb15d1775ebde76b113
Wayne <wynnee0110@gmail.com>
2026-06-20 08:17:18 +0800

    arcjet

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,19 +18,24 @@ app.set("trust proxy", true);
 // Arcjet configuration
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
   rules: [
     shield({
       mode: "LIVE",
     }),
     detectBot({
       mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "CATEGORY:PREVIEW",
+        "CATEGORY:MONITOR",
+      ],
     }),
     tokenBucket({
       mode: "LIVE",
-      refillRate: 10,
-      interval: 60,
-      capacity: 20,
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
     }),
   ],
 });
