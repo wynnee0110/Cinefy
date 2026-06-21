8edeef67e4dd0dd22fbc515537332893d1f2b4c5
Wayne <wynnee0110@gmail.com>
2026-06-21 18:49:06 +0800

    ui small changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -98,23 +98,23 @@ export default function Header({
 
   <span
     className={`inline-block transition-transform duration-300 ${
-      isGenreOpen ? "rotate-180" : "rotate-0"
+      isGenreOpen ? "rotate-180" : "rotate-[-360deg]"
     }`}
   >
-<svg
-  className="w-4 h-4"
-  viewBox="0 0 24 24"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path
-    d="M18 15L12 9L6 15"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  />
-</svg>
+    <svg
+      className="w-4 h-4"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 9L12 15L18 9"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   </span>
   
 </button>
